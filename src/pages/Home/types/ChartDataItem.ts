6a24d74c0202cb6d3de0bb70ababcaadc7551c17
jhdgo1225 <parkjh99365@gmail.com>
2025-07-13 02:50:12 +0900

    feat: 차트 버그 수정

---
+++ src/pages/Home/types/ChartDataItem.ts
@@ -0,0 +1,5 @@
+export interface ChartDataItem {
+  name: string;
+  points: number;
+  color: string;
+}
---
+++ src/pages/Home/types/CustomizedLabelProps.ts
@@ -0,0 +1,6 @@
+export interface CustomizedLabelProps {
+  x?: number;
+  y?: number;
+  width?: number;
+  value?: string | number;
+}
